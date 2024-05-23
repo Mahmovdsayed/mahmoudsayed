d26d6bbc32476681ab7ceba5544696de6da765a7
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-05-23 05:23:30 +0300

    Update projects.ts

--- functions/projects.ts
+++ functions/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Explore the world of movies and TV shows in a new light with our website powered by TMDb API. Dive into a vast database featuring details about films, actors, ratings, and more. Browse, save your favorite movies, and stay updated with the latest cinematic news. Immerse yourself in the seventh art effortlessly and enjoy a unique cinematic experience through our site",
     ImageUrl: "https://i.ibb.co/QPVXJxS/Slide-16-9-1.png",
-    projectLink: "https://movies-cafe.vercel.app/",
+    projectLink: "https://moviescafe.vercel.app/",
   },
   {
     title: "TikTok Video Downloader",
@@ -14,12 +14,12 @@ export const projects = [
       "https://res.cloudinary.com/dxvpvtcbg/image/upload/v1713852865/coydktlh5tcsxfjbumsp.jpg",
     projectLink: "https://tiksaverpro.vercel.app/",
   },
-  // {
-  //   title: "Fresh Cart (e-commerce)",
-  //   description:
-  //     "Explore a vast array of products across multiple categories with ease. Create an account to unlock personalized features like wishlists and carts. Seamlessly add items to your cart and enjoy secure online transactions. With detailed product descriptions and images, finding what you need has never been simpler. Elevate your shopping experience today with our intuitive website.",
-  //   ImageUrl:
-  //     "https://res.cloudinary.com/dxvpvtcbg/image/upload/v1715108336/yhtp638dsyc1iruielkh.svg",
-  //   projectLink: "https://e-commerce-bice-nine.vercel.app/",
-  // },
+  {
+    title: "Fresh Cart (e-commerce)",
+    description:
+      "Explore a vast array of products across multiple categories with ease. Create an account to unlock personalized features like wishlists and carts. Seamlessly add items to your cart and enjoy secure online transactions. With detailed product descriptions and images, finding what you need has never been simpler. Elevate your shopping experience today with our intuitive website.",
+    ImageUrl:
+      "https://res.cloudinary.com/dxvpvtcbg/image/upload/v1715108336/yhtp638dsyc1iruielkh.svg",
+    projectLink: "https://e-commerce-bice-nine.vercel.app/",
+  },
 ];

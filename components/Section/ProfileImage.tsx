821373ca4341db1c95c2e39b1aaf1074b596fda4
Mahmoud Sayed <138359247+Mahmovdsayed@users.noreply.github.com>
2024-08-18 02:08:34 +0300

    Update ProfileImage.tsx

--- components/Section/ProfileImage.tsx
+++ components/Section/ProfileImage.tsx
@@ -7,7 +7,7 @@ interface IProps {
 const ProfileImage = ({ }: IProps) => {
     return <>
         <div className="relative top-[-100px] md:top-[-150px]">
-            <Image className="w-32 h-32 md:w-48 md:h-48" isZoomed isBlurred src="https://i.pinimg.com/564x/0d/94/bd/0d94bd1c1bfec9157ff3323a43ff9cd3.jpg" />
+            <Image className="w-32 h-32 md:w-48 md:h-48"  isBlurred src="https://i.pinimg.com/564x/0d/94/bd/0d94bd1c1bfec9157ff3323a43ff9cd3.jpg" />
            
         </div>
         
